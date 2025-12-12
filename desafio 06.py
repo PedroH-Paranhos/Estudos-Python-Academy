786d3d612d3539909a60e48f2166822f08dfeb87
chaplinho20-cmd <chaplinho20@gmail.com>
2025-12-12 20:34:42 -0300

    style: alteração desafio 06

--- desafio 06.py
+++ desafio 06.py
@@ -2,4 +2,4 @@ numero = int(input("Digite um Número: "))
 
 print("Dobro = {}".format(numero * 2))
 print("Triplo = {}".format(numero * 3))
-print("Raiz Quadrada = {}".format(numero ** (1 / 2)))
+print("Raiz Quadrada = {:.4f}".format(numero ** (1 / 2)))
