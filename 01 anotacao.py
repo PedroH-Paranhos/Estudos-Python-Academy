985d2f5577bf93315da0549931f5f9e6b79b6b03
chaplinho20-cmd <chaplinho20@gmail.com>
2025-12-12 19:24:52 -0300

    feat: criação da anotação + desafio 26

---
+++ 01 anotacao.py
@@ -0,0 +1,6 @@
+#"""sequencia de envio para o git
+#1. git status
+#2. git add .
+#3. git status
+#4. git commit -m
+#5. git push origin master"""
---
+++ desafio 26.py
@@ -0,0 +1,13 @@
+#Faça um programa que leia uma frase pelo tecaldo e mostre:
+#quantas vezes aparece a letra "a" / em que posição aparece a primeira vez e ultima.
+frase = str(input('Digite uma frase: '))
+
+frase_ajeitada = frase.upper().strip()
+
+contagem = frase_ajeitada.count('A')
+primeira_posicao = frase_ajeitada.find('A')
+ultima_posicao = frase_ajeitada.rfind('A')
+
+print("A letra 'A' aparece {} vezes".format(contagem))
+print("A Primeira letra 'A' apareceu na posição {}".format(primeira_posicao))
+print("A última letra 'A' apareceu na posição {} ".format(ultima_posicao))
