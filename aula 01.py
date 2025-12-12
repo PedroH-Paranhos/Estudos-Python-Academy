ffbc892d128680ac15804c7ec4b1482e6c94e27b
Pedro H Paranhos <chaplinho20@gmail.com>
2025-12-12 12:02:28 -0300

    style: alteração aula 01.py
    dogo mudou o espaçamento entre as virgulas
    
    Co-authored-by: Diogo L. C. Felipe <dlcfelipp@gmail.com>

--- aula 01.py
+++ aula 01.py
@@ -1,4 +1,4 @@
 nome = input("Qual o seu nome? ")
 idade = input("Qual é sua idade? ")
 
-print("Seu nome é",nome,"e sua idade é",idade,". ")
+print("Seu nome é", nome, "e sua idade é", idade, ". ")
